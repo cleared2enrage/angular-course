bdf4a2edf13d8312c692759c139ec94ba1d44972
cleared2enrage <jason.a.giles@gmail.com>
2017-07-21 15:39:16 -0400

    Adds property directive to update style of element

--- src/app/server/server.component.ts
+++ src/app/server/server.component.ts
@@ -8,7 +8,22 @@ export class ServerComponent {
   serverId: number = 10;
   serverStatus: string = 'offline';
 
+  constructor() {
+    this.serverStatus = Math.random() > 0.5 ? 'online' : 'offline';
+  }
+
   getServerStatus() {
     return this.serverStatus;
+  }
+
+  getColor() {
+    switch (this.serverStatus) {
+      case 'online':
+        return 'green';
+      case 'offline':
+        return 'red';
+      default:
+        return 'black';
+    }
   }
 }
