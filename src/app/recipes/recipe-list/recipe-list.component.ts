600438cdffc735dd9cb7ec52ea3f84f6f966d62e
Cleared2Enrage <jason.a.giles@gmail.com>
2017-07-22 12:36:36 -0400

    Move list of recipes to recipe service

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit, EventEmitter, Output } from '@angular/core';
 import { Recipe } from "../recipe.model";
+import { RecipeService } from "../recipe.service";
 
 @Component({
   selector: 'app-recipe-list',
@@ -9,13 +10,12 @@ import { Recipe } from "../recipe.model";
 export class RecipeListComponent implements OnInit {
   @Output() recipeWasSelected = new EventEmitter<Recipe>();
   
-  recipes: Recipe[] = [
-    new Recipe('A Test Recipe', 'This is simply a test', 'http://lorempixel.com/400/200/')
-  ];
+  recipes: Recipe[];
 
-  constructor() { }
+  constructor(private recipeService: RecipeService) { }
 
   ngOnInit() {
+    this.recipes = this.recipeService.getRecipes();
   }
 
   onRecipeSelected(recipe: Recipe) {
--- src/app/recipes/recipe.service.ts
+++ src/app/recipes/recipe.service.ts
@@ -1,3 +1,11 @@
+import { Recipe } from "./recipe.model";
+
 export class RecipeService {
-    
+    private recipes: Recipe[] = [
+        new Recipe('A Test Recipe', 'This is simply a test', 'http://lorempixel.com/400/200/')
+    ];
+
+    getRecipes() {
+        return this.recipes.slice();
+    }
 }
