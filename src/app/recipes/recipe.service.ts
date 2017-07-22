428a3b5179d2637d9856ed0cc925ff240f85a848
Cleared2Enrage <jason.a.giles@gmail.com>
2017-07-22 12:55:46 -0400

    Add ingredients to recipe model

--- src/app/recipes/recipe.service.ts
+++ src/app/recipes/recipe.service.ts
@@ -1,11 +1,15 @@
 import { Recipe } from "./recipe.model";
 import { EventEmitter } from "@angular/core";
+import { Ingredient } from "../shared/ingredient.model";
 
 export class RecipeService {
     recipeSelected = new EventEmitter<Recipe>();
 
     private recipes: Recipe[] = [
-        new Recipe('A Test Recipe', 'This is simply a test', 'http://lorempixel.com/400/200/')
+        new Recipe('A Test Recipe', 'This is simply a test', 'http://lorempixel.com/400/200/', [
+            new Ingredient('Tomatoes', 4),
+            new Ingredient('Spaghetti', 1)
+        ])
     ];
 
     getRecipes() {
