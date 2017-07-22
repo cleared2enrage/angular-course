701576191a9d2989f4d5929675e97c190921578a
Cleared2Enrage <jason.a.giles@gmail.com>
2017-07-22 10:15:37 -0400

    Temporary solution to show and hide different sections of the app

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,4 +6,9 @@ import { Component } from '@angular/core';
   styles: ['./app.component.css']
 })
 export class AppComponent {
+  loadedFeature = 'recipe';
+
+  onNavigate(feature: string) {
+    this.loadedFeature = feature;
+  }
 }
